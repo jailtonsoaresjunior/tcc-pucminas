2aeb205ab57aba9136c64c67c884df724ab00fea
zenildo5 <zenildo5@yahoo.com.br>
2020-06-08 00:33:02 -0300

    Inclusão do SCA.Compliance.Dados

---
+++ SCA.Compliance.Dados/AcessoDados.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCA.Compliance.Dados
+{
+    public class AcessoDados
+    {
+        public List<string> ObterLista()
+        {
+            return new List<string> { "Compliance 1", "Compliance 2", "Compliance 3", "Compliance 4" };
+        }
+
+    }
+}
---
+++ SCA.Compliance.Dados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SCA.Compliance.Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
